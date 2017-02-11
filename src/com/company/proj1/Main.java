6badf17b27b44ffb9e43f0bc84b55f92e8f82dbe
Farrukh Rafiq <frafiq@student.cscc.edu>
2017-02-11 14:37:30 -0500

    project 1

--- src/com/company/proj1/Main.java
+++ src/com/company/proj1/Main.java
@@ -4,8 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/************************************************************************************************************
+ * Name : Frank Rafiq
+ * Instructor : Aurther N.
+ * Class: CISC 2994
+ * Project Name: Proj1
+ * Due date 2/7/17
+ * Description: This program uses ArrayList to add elements to an array, remove elements from an array,
+ *              update and print the element of the array.
+ */
 public class Main {
     static int inputMethod(){
+        int inputChoice;
         Scanner input = new Scanner(System.in);
         System.out.println("Please choose an option:");
         System.out.println();
@@ -18,8 +28,16 @@ public class Main {
         System.out.println("(4) List all tasks. ");
         System.out.println();
         System.out.println("(0) Exit.");
-        return Integer.parseInt(input.nextLine());
+        try {
+            inputChoice = Integer.parseInt(input.nextLine());
+            if (inputChoice == (int)inputChoice)
+                return inputChoice;
+        }
+        catch (Exception e){
+            System.out.println(" Please start again & enter a number between 0-4 as a choice. ");
+        }
 
+        return 0;
     }
     static void add(List<String> task, int addLocation, String addNewtask) {
 
@@ -39,7 +57,6 @@ public class Main {
         int location, choose;
         String inputTask;
         ArrayList<String> task = new ArrayList<String>();
-        //inputMethod();
         choose= inputMethod();
         while(choose != 0){
             if(choose == 1){
@@ -51,19 +68,14 @@ public class Main {
                 System.out.println("Enter the task: ");
                 inputTask = input.nextLine();
                 add(task, location, inputTask);
-                //inputMethod();
-                //choose= Integer.parseInt(input.nextLine());
-            }
+             }
             else if(choose == 2){
                 //remove
                 System.out.println("Current size of array is: " +task.size() );
                 System.out.println();
                 System.out.println("Enter The place from where u want the task removed: ");
                 location= Integer.parseInt(input.nextLine());
-                //System.out.println("Enter the task: ");
-                //inputTask = input.nextLine();
                 remove(task,location);
-                //inputMethod();
                 //remove
             }
             else if (choose == 3){
@@ -84,12 +96,14 @@ public class Main {
                 }
             }
             else{
-                // inputMethod();
-                //continue;
+                //default
+
             }
+            //pause before program starts over again.
+            System.out.println();
+            System.out.println("Please hit enter to continue:");
+            inputTask = input.nextLine();
             //call input method again;
-            // wait to continue logic
-            //inputMethod();
             choose=  inputMethod();
         }
 
