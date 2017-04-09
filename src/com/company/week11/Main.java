1b7e14313a2b1dec12979355787dad05f11e804a
frafiq <rafiq.farrukh@gmail.com>
2017-04-09 00:03:02 -0400

    week11 exercise

--- src/com/company/week11/Main.java
+++ src/com/company/week11/Main.java
@@ -63,23 +63,92 @@ interface WeatherDataListener{
 
 }
 interface WeatherSensor{
-    void addListene(WeatherDataListener listener);
-    void
+    void addListener(WeatherDataListener listener);
+    void updateListeners(String data);
 }
-class TempSemsors implements WeatherSensor {
+class TempSensor implements WeatherSensor {
     List<WeatherDataListener> listeners = new ArrayList<>();
     private String temperature;
     public void updateTemperature(){
         double measuredTemp = new Random().nextDouble()*100;
+        temperature = "Temperature: " + measuredTemp;
+        updateListeners(temperature);
+    }
+
+
+
+    @Override
+    public void addListener(WeatherDataListener listener) {
+        listeners.add(listener);
+
+    }
+
+    @Override
+    public void updateListeners(String data) {
+        for (WeatherDataListener listener: listeners) {
+            listener.update(data);
+        }
+
     }
 }
+class HumidSensor implements WeatherSensor {
+    List<WeatherDataListener> listeners = new ArrayList<>();
+    private String humidity;
+
+    public void updateHumidity() {
+        double measuredTemp = new Random().nextDouble();
+        humidity = "Humidity: " + measuredTemp;
+        updateListeners(humidity);
+    }
+
+    @Override
+    public void addListener(WeatherDataListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void updateListeners(String data) {
+        for (WeatherDataListener listener : listeners) {
+            listener.update(data);
+        }
+    }
+}
+class PressureSensor implements WeatherSensor {
+    List<WeatherDataListener> listeners = new ArrayList<>();
+    private String pressure;
+
+    public void updatePressure() {
+        double measuredTemp = new Random().nextDouble();
+        pressure = "Pressure: " + measuredTemp;
+        updateListeners(pressure);
+    }
+
+    @Override
+    public void addListener(WeatherDataListener listener) {
+        listeners.add(listener);
+    }
+
+    @Override
+    public void updateListeners(String data) {
+        for (WeatherDataListener listener: listeners) {
+            listener.update(data);
+        }
+    }
+}
+
 // Listener
 class WeatherStation implements WeatherDataListener{
-    List<String> log new ArrayList<>;
+    List<String> log = new ArrayList<>();
 
     @Override
     public void update(String data) {
-
+        System.out.println("New weather data: " + data);
+        log.add(data);
+    }
+    public void displayLog() {
+        for (String weatherItem: log) {
+            System.out.println(weatherItem);
+        }
     }
 }
 
@@ -93,10 +162,22 @@ public class Main {
 //        friends.add("pete");
 //        StringFilter bFilter = FilterCreator.startsWithFilter("b");
 //        List
-          WeatherDataListener myWeatherStation = new WeatherStation();
-          TempSemsors tempSensor =new TempSemsors();
-          humidSensors
+          WeatherStation myWeatherStation = new WeatherStation();
+          TempSensor tempSensor = new TempSensor();
+          HumidSensor humidSensor = new HumidSensor();
+          PressureSensor pressureSensor = new PressureSensor();
+            tempSensor.addListener(myWeatherStation);
+            humidSensor.addListener(myWeatherStation);
+            pressureSensor.addListener(myWeatherStation);
 
-    }
+            System.out.println("Simulate sensor updates");
+            tempSensor.updateTemperature();
+            humidSensor.updateHumidity();
+            tempSensor.updateTemperature();
+            pressureSensor.updatePressure();
+
+            System.out.println("All weather events");
+            myWeatherStation.displayLog();
+        }
 
 }
